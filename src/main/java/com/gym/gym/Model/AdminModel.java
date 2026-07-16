bb210a322306e007a1a3091df28e9bd211803d74
Bratt Diaz <bratt565699@gmail.com>
2026-07-16 15:15:46 -0500

    se agrega el modelo y repositorio de admin

---
+++ src/main/java/com/gym/gym/Model/AdminModel.java
@@ -0,0 +1,34 @@
+package com.gym.gym.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "apellido", nullable = true)
+    private String apellido;
+    @Column(name = "correo", nullable = false)
+    private String correo;
+    @Column(name = "nombre_de_gym", nullable = false)
+    private String NombreDeGym;
+    @Column(name = "contraseña", nullable = false)
+    private String Contraseña;
+   
+
+}
---
+++ src/main/java/com/gym/gym/Respository/AdminRespository.java
@@ -0,0 +1,12 @@
+package com.gym.gym.Respository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gym.gym.Model.AdminModel;
+
+public interface AdminRespository extends JpaRepository<AdminModel, Long> {
+    boolean existsByEmail(String correo);
+    Optional<AdminModel> findByCorreo(String correo);
+}
