a16a50ceed7f3ec7cc4fdb1dc9abb6c9119ea481
Bratt Diaz <bratt565699@gmail.com>
2026-07-24 21:37:09 -0500

    se agrega la anotacion jsonignorepropierties para la seguridad del JSON en no permitir traer campos que no esten registrados en el sistema es decir, no permitir propiedades desconocidas

--- src/main/java/com/gym/gym/DTO/Request/MiembroRequestDTO.java
+++ src/main/java/com/gym/gym/DTO/Request/MiembroRequestDTO.java
@@ -2,12 +2,15 @@ package com.gym.gym.DTO.Request;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = false)
 public class MiembroRequestDTO {
     private String nombre;
     private String apellido;
